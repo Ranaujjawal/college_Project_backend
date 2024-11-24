d12025ec849c101ec080bd1872169e65a6599788
ujjawal <ranaujjawal692@gmail.com>
2024-11-24 17:25:21 +0530

    chcking

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ import { setupWebSocket } from './controller/websocket.js';
 import RedisStore from "connect-redis"
 import session from "express-session"
 import {createClient} from "redis"
-// import Redis from 'ioredis'
+ import Rediss from 'ioredis'
 import Redis from 'redis'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import dotenv from 'dotenv'
-
+const client = new Rediss("rediss://default:AbTzAAIjcDE0ZmU3NjE0MjY4ZmQ0YTQ0YjE1MDlmYzJjMDFkZGMzY3AxMA@logical-gecko-46323.upstash.io:6379");
 dotenv.config()
 const app = express();
 
@@ -26,7 +26,7 @@ let redisClient = createClient()
   
 
 let redisStore = new RedisStore({
-  client: redisClient
+  client: client
 })
 
 const allowedOrigin = process.env.CLIENT_URL;
@@ -42,29 +42,31 @@ app.use(cors({
   credentials: true, //process.env.CLIENT_URL
 }));
 app.set("trust proxy", 1);
-app.use(
-  session({
-    secret: 'your-secret-key', 
-    resave: false,
-    saveUninitialized: true,
-    proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
-    name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
-    cookie: {
-      secure: true, // required for cookies to work on HTTPS
-      httpOnly: false,
-      sameSite: 'none'
-    }
-  })
-);
-// session({
-//   store: redisStore,
-//   secret: 'your_secret_key',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { 
-    
-//     secure: false } 
-// })
+// app.use(
+//   session({
+//     secret: 'your-secret-key', 
+//     resave: false,
+//     saveUninitialized: true,
+//     proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
+//     name: 'MyCoolWebAppCookieName', // This needs to be unique per-host.
+//     cookie: {
+//       secure: true, // required for cookies to work on HTTPS
+//       httpOnly: false,
+//       sameSite: 'none'
+//     }
+//   })
+// );
+app.use(session({
+  store: redisStore,
+  secret: 'your_secret_key',
+  resave: false,
+  saveUninitialized: false,
+  proxy: true, // Required for Heroku & Digital Ocean (regarding X-Forwarded-For)
+  name: 'MyCoolWebAppCookieName', 
+  cookie: { 
+    httpOnly: false,
+    secure: false } 
+}))
 
 
 // Routes
